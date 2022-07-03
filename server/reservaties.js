cc4f0ac135448691d4ef60b5ce63e2f9c5e7fc45
davurt <david.urting@gmail.com>
2022-07-03 23:50:07 +0200

    Werkende backend. Al getest, maar nog verder door te testen. Functioneel volledig.

--- server/reservaties.js
+++ server/reservaties.js
@@ -2,7 +2,7 @@ import { v4 as uuidv4, v4 } from "uuid";
 
 function reserveer(username, vouchercode, kamerIds) {
     // TODO: hier moet natuurlijk code komen voor het effectief bewaren van de reservatie. Maar dat is out of scope.
-    console.log(`Reservatie van ${kamerIds.length} kamers voor gebruiker ${username} met vouchercode ${vouchercode} (kan leeg zijn)...`);
+    console.log(`Reservatie van ${kamerIds.length} kamers voor gebruiker ${username} met (optionele) vouchercode ${vouchercode} (kan leeg zijn)...`);
 
     let reservatienummer = v4();
     return reservatienummer;
